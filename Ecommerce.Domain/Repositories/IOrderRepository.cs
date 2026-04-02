7c3093519028416617783c51967b6fcef526adb7
joao <124461386+joaohnt@users.noreply.github.com>
2026-04-02 18:40:08 -0300

    feat: define IOrderRepository and ICustomerRepository interfaces (#4)

---
+++ Ecommerce.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Repositories;
+
+public interface IOrderRepository
+{
+    Task AddAsync(Order order);
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task<Order> GetByIdAsync(int orderId);
+    Task UpdateAsync(Order order);
+    Task DeleteAsync(int orderId);
+}

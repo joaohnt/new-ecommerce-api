79a9068d54be5d447d34d29324ed68c30350299f
joaohnt <jpedrooo.cg@gmail.com>
2026-04-03 02:26:48 -0300

    feat: add error handling for api

---
+++ Ecommerce.API/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Exceptions;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var problem = exception switch
+        {
+            InvalidOperationException => new ProblemDetails
+            {
+                Title = "Ocorreu um erro.",
+                Detail = exception.Message,
+                Status = StatusCodes.Status400BadRequest
+            },
+            ArgumentOutOfRangeException => new ProblemDetails
+            {
+                Title = "Ocorreu um erro.",
+                Detail = exception.Message,
+                Status = StatusCodes.Status400BadRequest
+            },
+            ArgumentNullException => new ProblemDetails
+            {
+                Title = "Ocorreu um erro.",
+                Detail = exception.Message,
+                Status = StatusCodes.Status400BadRequest
+            },
+            KeyNotFoundException => new ProblemDetails
+            {
+                Title = "Ocorreu um erro.",
+                Detail = exception.Message,
+                Status = StatusCodes.Status404NotFound
+            },
+            ArgumentException => new ProblemDetails
+            {
+                Title = "Ocorreu um erro.",
+                Detail = exception.Message,
+                Status = StatusCodes.Status400BadRequest
+            },
+            _ => new ProblemDetails
+            {
+                Title = "Internal server error",
+                Detail = "Ocorreu um erro inesperado.",
+                Status = StatusCodes.Status500InternalServerError
+            }
+        };
+
+        httpContext.Response.StatusCode = problem.Status!.Value;
+        await httpContext.Response.WriteAsJsonAsync(problem, cancellationToken);
+
+        return true;
+    }
+}

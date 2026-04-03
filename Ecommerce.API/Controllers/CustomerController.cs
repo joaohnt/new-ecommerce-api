22f8935358bed7a72b4b2a9a74a2a4dba9a52d61
joaohnt <jpedrooo.cg@gmail.com>
2026-04-03 01:27:00 -0300

    feat: implement CustomerController and configure swagger

---
+++ Ecommerce.API/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using Ecommerce.Application.Customer.Commands;
+using Ecommerce.Application.Customer.DTOs;
+using Ecommerce.Application.Customer.Queries;
+using Ecommerce.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomerController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public CustomerController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<IEnumerable<CustomerDto>>> GetAllAsync()
+    {
+        var customers = await _mediator.Send(new GetAllCustomersQuery());
+        return Ok(customers);
+    }
+
+    [HttpGet]
+    [Route("{customerId}")]
+    public async Task<ActionResult<CustomerDto>> GetByIdAsync(int customerId)
+    {
+        var customer = await _mediator.Send(new GetCustomerByIdQuery{CustomerId = customerId});
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<CustomerDto>> CreateAsync([FromBody] CreateCustomerCommand command)
+    {
+        var customer = await _mediator.Send(command);
+        return Ok(customer);
+    }
+
+    [HttpDelete]
+    [Route("{customerId}")]
+    public async Task<ActionResult> DeleteAsync(int customerId)
+    {
+        await _mediator.Send(new DeleteCustomerCommand{CustomerId = customerId});
+        return NoContent();
+    }
+}
